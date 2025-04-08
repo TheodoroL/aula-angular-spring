fe2a3766f54e6fc5874e95b7e18d65e0d8ce0ffd
Lucas <lucastheodoromourofernandes@gmail.com>
2025-04-08 12:11:53 -0400

    adicionando o service selecionar e fazendo a listagem de clientes

--- api-front/src/app/principal/principal.component.ts
+++ api-front/src/app/principal/principal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Client } from '../model/Client';
+import { ClientService } from '../service/client.service';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
@@ -9,5 +10,12 @@ export class PrincipalComponent {
 
   public btnCadestro: boolean = true;
 
+  public clientes: Client[] = [];
 
+  constructor(private service: ClientService) { }
+
+  public selecionar(): void {
+    this.service.selecionar()
+      .subscribe(value => this.clientes = value);
+  }
 }
--- api-front/src/app/service/client.service.ts
+++ api-front/src/app/service/client.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from '../model/Client';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,9 @@ export class ClientService {
 
   constructor(private client: HttpClient) {
 
+  }
+  //vai fazer um request do tipo get 
+  public selecionar(): Observable<Client[]> {
+    return this.client.get<Client[]>(this.urlApi);
   }
 }
